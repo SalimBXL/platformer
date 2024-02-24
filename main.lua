f4176686e6fa85bc5e8d2431473128dad9ed647d
SalimBXL <el.salim.salim@gmail.com>
2024-02-24 21:06:45 +0100

    platformer

---
+++ main.lua
@@ -0,0 +1,44 @@
+function love.load()
+    wf = require 'libraries/windfield/windfield'
+    world = wf.newWorld(0, 800, false)
+
+    world:addCollisionClass('Platform')
+    world:addCollisionClass('Player' --[[, {ignores = {'Platform'}}]])
+    world:addCollisionClass('Danger')
+
+    -- Platform definition
+    platform = world:newRectangleCollider(250,400,300,100, {collision_class = "Platform"})
+    platform:setType('static')
+
+    -- Player definition
+    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "Player"})
+    player:setFixedRotation(true)
+    player.speed = 240
+
+    -- Danger definition
+    dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "Danger"})
+    dangerZone:setType('static')
+end
+
+function love.update(dt)
+    world:update(dt)
+
+    -- Player movements
+    if player.body then
+        local px, py = player:getPosition()
+        if love.keyboard.isDown('right') then player:setX(px + player.speed * dt) end
+        if love.keyboard.isDown('left') then player:setX(px - player.speed * dt) end
+        if player:enter('Danger') then player:destroy() end
+    end
+end
+
+function love.draw()
+    world:draw()
+end
+
+-- Manage Jump for player
+function love.keypressed(key)
+    if key == 'up' then
+        player:applyLinearImpulse(0, -7000)
+    end
+end
