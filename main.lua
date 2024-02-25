507551f0b41d7b3ec5a0a6800c2b497a66972164
SalimBXL <el.salim.salim@gmail.com>
2024-02-25 05:46:06 +0100

    levels

--- main.lua
+++ main.lua
@@ -5,20 +5,25 @@ function love.load()
     anim8 = require 'libraries/anim8/anim8'
     sti = require 'libraries/Simple-Tiled-Implementation/sti'
     cameraFile = require 'libraries/hump/camera'
+    wf = require 'libraries/windfield/windfield'
 
     cam = cameraFile()
 
-    -- Animations
+    -- Sprites
     sprites = {}
     sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+    sprites.enemySheet = love.graphics.newImage('sprites/enemySheet.png')
+    
+    -- Animations
     local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    local enemyGrid = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
     animations = {}
     animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
     animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
     animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+    animations.enemy = anim8.newAnimation(enemyGrid('1-2', 1), 0.03)
 
     -- World definition
-    wf = require 'libraries/windfield/windfield'
     world = wf.newWorld(0, 800, false)
     world:setQueryDebugDrawing(true)
 
@@ -30,22 +35,38 @@ function love.load()
     -- Player definition
     require('player')
 
+    -- Ennemies definition
+    require('enemy')
+
     -- Danger zones definition
     --dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "Danger"})
     --dangerZone:setType('static')
 
     -- Map Level
     platforms = {}
-    loadMap()
+    flagX = 0
+    flagY = 0
+    currentLevel = "level1"
+    loadMap(currentLevel)
 end
 
 function love.update(dt)
     world:update(dt)
     gameMap:update(dt)
     playerUpdate(dt)
+    updateEnemies(dt)
 
     local px, py = player:getPosition()
     cam:lookAt(px, love.graphics.getHeight()/2)
+
+    local colliders = world:queryCircleArea(flagX, flagY, 10, {'Player'})
+    if #colliders > 0 then
+        if currentLevel == "level1" then
+            loadMap("level2")
+        elseif currentLevel == "level2" then
+            loadMap("level1")
+        end
+    end
 end
 
 
@@ -54,6 +75,7 @@ function love.draw()
         gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
         world:draw()
         drawPlayer()
+        drawEnemies()
     cam:detach()
 end
 
@@ -83,11 +105,53 @@ function spawnPlatform(x, y, width, height)
     end
 end
 
-function loadMap()
-    gameMap = sti("maps/level1.lua")
-    local objects = gameMap.layers["Platforms"].objects
-    for i, obj in pairs(objects) do
+function destroyAll()
+    -- Platforms
+    local i = #platforms
+    while i > -1 do
+        if platforms[i] ~= nil then
+            platforms[i]:destroy()
+        end
+        table.remove(platforms, i)
+        i = i - 1
+    end
+    -- Ennemies
+    local i = #enemies
+    while i > -1 do
+        if enemies[i] ~= nil then
+            enemies[i]:destroy()
+        end
+        table.remove(enemies, i)
+        i = i - 1
+    end
+end
+
+function loadMap(mapName)
+    currentLevel = mapName
+    destroyAll()
+    player:setPosition(300, 100)
+    gameMap = sti("maps/" .. mapName .. ".lua")
+
+    -- Platforms
+    local platformObjects = gameMap.layers["Platforms"].objects
+    for i, obj in pairs(platformObjects) do
+        --print("x:"..obj.x.." y:"..obj.y)
         spawnPlatform(obj.x, obj.y, obj.width, obj.height)
+    end
+
+    --Enemies
+    local enemyObjects = gameMap.layers["Enemies"].objects
+    for i, obj in pairs(enemyObjects) do
+        --print("x:"..obj.x.." y:"..obj.y)
+        spawnEnemy(obj.x, obj.y, obj.width, obj.height)
+    end
+
+    --Flags
+    local flagObjects = gameMap.layers["Flag"].objects
+    for i, obj in pairs(flagObjects) do
+        --print("x:"..obj.x.." y:"..obj.y)
+        flagX = obj.x
+        flagY = obj.y
     end
 end
 
