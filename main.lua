f851962c3e1757e4411e6594bf9a6c9ba30ca7d5
SalimBXL <el.salim.salim@gmail.com>
2024-02-25 01:58:20 +0100

    player animations

--- main.lua
+++ main.lua
@@ -1,7 +1,21 @@
 function love.load()
+
+    -- Animations
+    anim8 = require 'libraries/anim8/anim8'
+    sprites = {}
+    sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+    local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    animations = {}
+    animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
+    animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
+    animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+
+    -- World definition
     wf = require 'libraries/windfield/windfield'
     world = wf.newWorld(0, 800, false)
+    world:setQueryDebugDrawing(true)
 
+    -- World 's collisons
     world:addCollisionClass('Platform')
     world:addCollisionClass('Player' --[[, {ignores = {'Platform'}}]])
     world:addCollisionClass('Danger')
@@ -11,11 +25,15 @@ function love.load()
     platform:setType('static')
 
     -- Player definition
-    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "Player"})
+    player = world:newRectangleCollider(360, 100, 40, 100, {collision_class = "Player"})
     player:setFixedRotation(true)
     player.speed = 240
+    player.animation = animations.idle
+    player.isMoving = false
+    player.direction = 1
+    player.grounded = true
 
-    -- Danger definition
+    -- Danger zoneS definition
     dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "Danger"})
     dangerZone:setType('static')
 end
@@ -25,20 +43,71 @@ function love.update(dt)
 
     -- Player movements
     if player.body then
+        getPlayerColliders()
+        player.isMoving = false
         local px, py = player:getPosition()
-        if love.keyboard.isDown('right') then player:setX(px + player.speed * dt) end
-        if love.keyboard.isDown('left') then player:setX(px - player.speed * dt) end
+
+        if love.keyboard.isDown('right') then
+            player:setX(px + player.speed * dt)
+            player.isMoving = true
+            player.direction = 1
+        end
+        if love.keyboard.isDown('left') then 
+            player:setX(px - player.speed * dt)
+            player.isMoving = true
+            player.direction = -1
+        end
         if player:enter('Danger') then player:destroy() end
     end
+
+    if player.grounded then
+        if player.isMoving then
+            player.animation = animations.run
+        else
+            player.animation = animations.idle
+        end
+    else 
+        player.animation = animations.jump
+    end
+    
+    player.animation:update(dt)
 end
+
 
 function love.draw()
     world:draw()
+
+    -- Player
+    local px, py = player:getPosition()
+    player.animation:draw(sprites.playerSheet, px, py, nil, (0.25 * player.direction), 0.25, 130, 300)
+end
+
+function getPlayerColliders()
+    local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 50, 2, {'Platform'})
+    if #colliders > 0 then 
+        player.grounded = true
+    else
+        player.grounded = false
+    end
+    return colliders
 end
 
 -- Manage Jump for player
 function love.keypressed(key)
     if key == 'up' then
-        player:applyLinearImpulse(0, -7000)
+        if player.grounded then
+            player:applyLinearImpulse(0, -4000)
+            player.animation = animations.jump
+        end
+    end
+end
+
+
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        local colliders = world:queryCircleArea(x, y, 200, {'Platform', 'Danger'})
+        for i,c in ipairs(colliders) do
+            c:destroy()
+        end
     end
 end
